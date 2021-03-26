1763ca7f7f6cdff3c03fd71aef54100a2b90d7d5
Boren <53597966+borenx1@users.noreply.github.com>
2021-03-26 23:17:13 +1300

    Add prop to AddEditDialog component to set max width

--- src/components/AddEditDialog.js
+++ src/components/AddEditDialog.js
@@ -1,8 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+
+const useStyles = makeStyles(theme => ({
+  content: props => ({
+    maxWidth: props.contentMaxWidth,
+  }),
+}));
 
 /**
  * A dialog to add or edit an object.
@@ -15,13 +22,15 @@ import DialogTitle from '@material-ui/core/DialogTitle';
  * - onEnter - Function called before the dialog is opened.
  * - onReset - Function called when the "reset" button is clicked.
  * - onSubmit - Function called when the "Add/Edit" button is clicked.
- * - dividers - Display the DialogContent dividers if true
+ * - dividers - Display the DialogContent dividers if true.
+ * - contentMaxWidth - The maximum width of the dialog content.
  */
 function AddEditDialog(props) {
+  const classes = useStyles(props);
   return (
     <Dialog open={props.open} onClose={props.onClose} onEnter={props.onEnter}>
       <DialogTitle>{ `${props.edit ? 'Edit' : 'Add'} ${props.objectName || 'Item'}` }</DialogTitle>
-      <DialogContent dividers={Boolean(props.dividers)}>
+      <DialogContent dividers={Boolean(props.dividers)} className={classes.content}>
         { props.children }
       </DialogContent>
       <DialogActions>
