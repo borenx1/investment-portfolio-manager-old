9d1a236fed08507773b8221f888fb1370fcc1b06
Boren <53597966+borenx1@users.noreply.github.com>
2021-03-30 02:13:02 +1300

    refactor: Remove redundant React.Fragment

--- src/features/accounts/AccountSettings.tsx
+++ src/features/accounts/AccountSettings.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
@@ -7,32 +5,22 @@ import AccountMainSettings from './AccountMainSettings';
 import AccountAssetsSettings from './AccountAssetsSettings';
 import AccountJournalsSettings from './AccountJournalsSettings';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-
-  },
-}));
-
 function AccountSettings() {
-  const classes = useStyles();
-
   return (
-    <React.Fragment>
-      <Container className={classes.root}>
-        <Box my={2}>
-          <Typography variant="h4">Account Settings</Typography>
-        </Box>
-        <Box my={2}>
-          <AccountMainSettings />
-        </Box>
-        <Box my={2}>
-          <AccountAssetsSettings />
-        </Box>
-        <Box my={2}>
-          <AccountJournalsSettings />
-        </Box>
-      </Container>
-    </React.Fragment>
+    <Container>
+      <Box my={2}>
+        <Typography variant="h4">Account Settings</Typography>
+      </Box>
+      <Box my={2}>
+        <AccountMainSettings />
+      </Box>
+      <Box my={2}>
+        <AccountAssetsSettings />
+      </Box>
+      <Box my={2}>
+        <AccountJournalsSettings />
+      </Box>
+    </Container>
   );
 }
 
