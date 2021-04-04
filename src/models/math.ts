9dafa271e83a6920613cac489c8a1de6b9d23d79
Boren <53597966+borenx1@users.noreply.github.com>
2021-04-04 13:14:30 +1200

    refactor: Rename math functions argument decimals to precision

--- src/models/math.ts
+++ src/models/math.ts
@@ -1,18 +1,18 @@
-function decimalFactor(decimals: number): number {
-  if (decimals < 0 || isNaN(decimals)) {
+function decimalFactor(precision: number): number {
+  if (precision < 0 || isNaN(precision)) {
     return NaN;
   }
-  return Math.pow(10, Math.trunc(decimals));
+  return Math.pow(10, Math.trunc(precision));
 }
 
-export function roundDown(value: number, decimals: number): number {
-  const factor = decimalFactor(decimals)
+export function roundDown(value: number, precision: number): number {
+  const factor = decimalFactor(precision)
   if (isNaN(factor)) {
     return value;
   }
   return Math.floor(value * factor) / factor;
 }
 
-export function multiply(n1: number, n2: number, decimals: number) {
-  return roundDown(n1 * n2, decimals);
+export function multiply(n1: number, n2: number, precision: number) {
+  return roundDown(n1 * n2, precision);
 }
