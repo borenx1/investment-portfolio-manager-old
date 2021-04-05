dad766e1d6bca31d0ca5330a8f673d7b0ba1dcd4
Boren <53597966+borenx1@users.noreply.github.com>
2021-04-05 22:57:53 +1200

    test: Add tests for addTransactionOrdered

--- src/models/accountFunctions.test.ts
+++ src/models/accountFunctions.test.ts
@@ -1,5 +1,5 @@
 import { createTradingJournal, Journal, Transaction, Asset } from "./account";
-import { dateToString, getDecimalColumnPrecision, isRightAlignJournalColumnType, journalColumnRoleDisplay, transactionDataDisplay } from "./accountFunctions";
+import { addTransactionOrdered, dateToString, getDecimalColumnPrecision, isRightAlignJournalColumnType, journalColumnRoleDisplay, transactionDataDisplay } from "./accountFunctions";
 
 describe('dateToString', () => {
   test('expected (with time)', () => {
@@ -47,9 +47,9 @@ describe('isRightAlignJournalColumnType', () => {
   });
 });
 
-describe('testTransactions', () => {
+describe('testTransaction', () => {
   const testTransaction: Transaction = {
-    date: '01/01/2021',
+    date: '2021-01-01',
     base: 'BTC',
     baseAmount: 2,
     quote: 'USD',
@@ -123,5 +123,63 @@ describe('testTransactions', () => {
       expect(transactionDataDisplay(testTransaction, 'quoteAmount', testJournalPrecision, testAssets)).toEqual('90000.000');
       expect(transactionDataDisplay(testTransaction, 'price', testJournalPrecision, testAssets)).toEqual('45000.00000');
     });
+  });
+});
+
+describe('addTransactionOrdered', () => {
+  const testTransaction: Transaction = {
+    date: '2000-01-01',
+    base: 'BTC',
+    baseAmount: 1,
+    quote: 'USD',
+    quoteAmount: 10,
+    feeBase: 0,
+    feeQuote: 0,
+    notes: 'Test transaction',
+    extra: {},
+  };
+  const testTransactions: Transaction[] = [testTransaction];
+  test('add newest transaction', () => {
+    addTransactionOrdered(testTransactions, {
+      ...testTransaction,
+      date: '2000-01-31',
+      notes: 'Test transaction 2',
+    });
+    // [1, 2]
+    expect(testTransactions[0]?.notes).toEqual('Test transaction');
+    expect(testTransactions[1]?.notes).toEqual('Test transaction 2');
+  });
+  test('add in between transaction', () => {
+    addTransactionOrdered(testTransactions, {
+      ...testTransaction,
+      date: '2000-01-30',
+      notes: 'Test transaction 3',
+    });
+    // [1, 3, 2]
+    expect(testTransactions[1]?.notes).toEqual('Test transaction 3');
+    expect(testTransactions[2]?.notes).toEqual('Test transaction 2');
+  });
+  test('add oldest transaction', () => {
+    addTransactionOrdered(testTransactions, {
+      ...testTransaction,
+      date: '1999-12-12',
+      notes: 'Test transaction 4',
+    });
+    // [4, 1, 3, 2]
+    expect(testTransactions[0]?.notes).toEqual('Test transaction 4');
+    expect(testTransactions[1]?.notes).toEqual('Test transaction');
+    expect(testTransactions[3]?.notes).toEqual('Test transaction 2');
+  });
+  test('add transaction with time', () => {
+    addTransactionOrdered(testTransactions, {
+      ...testTransaction,
+      date: '2000-01-30 18:00:00',
+      notes: 'Test transaction 5',
+    });
+    // [4, 1, 3, 5, 2]
+    expect(testTransactions[0]?.notes).toEqual('Test transaction 4');
+    expect(testTransactions[2]?.notes).toEqual('Test transaction 3');
+    expect(testTransactions[3]?.notes).toEqual('Test transaction 5');
+    expect(testTransactions[4]?.notes).toEqual('Test transaction 2');
   });
 });
